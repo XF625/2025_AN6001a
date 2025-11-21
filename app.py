f6f155c06da8be8514c056620caf9165725b5454
XF_625 <114678711+XF625@users.noreply.github.com>
2025-11-21 01:56:15 +0000

    1

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask,request,render_template
 
 app = Flask(__name__)
-
-
 @app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+
+@app.route("/main",methods=["GET","POST"])
 def main():
     q = request.form.get("q")
     print(q)
